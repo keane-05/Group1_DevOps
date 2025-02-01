3fc1a43684e90a174fc152689c83933dfd7d43e8
JoshuaJed <joshuajedabiog@gmail.com>
2025-02-01 20:43:55 +0800

    added some text lol

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import {Text, Pressable, StyleSheet, View} from "react-native"
-
 import Card from '@/components/card'
 
-export default function Info() { 
+export default function About() { 
     return (
-        <View/>
+        <View style={Styles.container}>
+            <Text style={Styles.text}>Made by a few people ig</Text>
+        </View>
     )
 }
+
+
+const Styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+
+    text: {
+        fontSize: 20, 
+        color: "#ababab"
+    }
+})
