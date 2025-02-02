208e7fd370fddcb75b24181781476583d4f7444d
JoshuaJed <joshuajedabiog@gmail.com>
2025-02-02 14:11:32 +0800

    Added a button

--- app/payment.tsx
+++ app/payment.tsx
@@ -1,5 +1,34 @@
 import React from 'react'
+import {Text, Pressable, StyleSheet, View} from "react-native"
+import { Image } from 'expo-image'
+import { useLocalSearchParams,  useRouter} from "expo-router"
+import { drinks } from './(tabs)/card'
+import Button from '@/components/buttons'
 
-export default function Info() { 
-        return<></>
+export default function payment() { 
+        const { id } = useLocalSearchParams();
+        const drink = drinks.find(d => d.id === Number(id));  // Find the matching drink
+        const router = useRouter(); 
+
+        const changePage = () => {
+                router.push({
+                  pathname: "../qrcode",
+                  params: {id: id}
+                }); 
+        };
+
+        return<>
+              <View style={styles.buttonContainer}>
+                <Button label="Order Now" theme="primary" onPress={changePage} />
+              </View>
+        </>
 }
+
+const styles = StyleSheet.create({
+        buttonContainer: {
+                position: "absolute",
+                bottom: 40,
+                left: 20,
+                right: 20,
+        }
+})
